8da3d41713cfb8fc92a325aec8df3f7054325a59
SHARJEEL-DS <sharjeelsohail697@gmail.com>
2023-06-16 00:16:46 +0600

    project setup

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Chat App</title>
+        <title>sharjeel</title>
         <meta name="Chat-app" content="/" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
